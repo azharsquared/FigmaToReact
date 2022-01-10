ca1d01b3e5644afbb62a3663a1a7e0916fb28997
azharsqaured <azharsquared@gmail.com>
2022-01-10 07:32:40 +0530

    navbar

---
+++ src/container/whatGPT3/WhatGTP3.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatGTP3.css';
+
+const WhatGTP3 = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default WhatGTP3
